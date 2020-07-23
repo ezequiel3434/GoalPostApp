c474249c54359217a63d505c2e5c6cbc687a7053
Ezequiel <ezequiel3434@gmail.com>
2020-07-23 16:06:34 -0300

    Initial Commit

---
+++ GoalPostApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoalPostApp
+//
+//  Created by Ezequiel Parada Beltran on 23/07/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
